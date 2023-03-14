b2e174700563b50989b53591f4681d09a108398f
levanphat1993 <levanphatit1993@gmail.com>
2023-03-15 00:23:13 +0700

    VueJs Events Mouseover And Mouseout

---
+++ lesson.11/js/app.js
@@ -0,0 +1,21 @@
+let vm = new Vue({
+    el: '#databinding',
+    data: {
+        num1: 100,
+        num2: 200,
+        total: '',
+        styleobj: {
+            with: "100px",
+            hegiht: "100px",
+            backgroundColor: "red"
+        }
+    },
+    methods: {
+        changebgcolor : function () {
+            this.styleobj.backgroundColor = "green";
+        },
+        originalcolor : function () {
+            this.styleobj.backgroundColor = "red";
+        }
+    }
+});

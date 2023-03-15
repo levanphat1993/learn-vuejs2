5798f9b75d2a8cdbc01ff8f952742cf61f38ba6a
levanphat1993 <levanphatit1993@gmail.com>
2023-03-15 18:10:23 +0700

    VueJs Transition & Animation

---
+++ lesson.20/js/app.js
@@ -0,0 +1,13 @@
+let vm = new Vue({
+    el: '#databinding',
+    data: {
+        show: true,
+        styleobj: {
+            fontSize: '30px',
+            color: 'red'
+        }
+    },
+    methods : {
+        
+    }
+});

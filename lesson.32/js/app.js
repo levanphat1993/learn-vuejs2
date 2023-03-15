6c5174c6be553918a2749c886716393a4bea0857
levanphat1993 <levanphatit1993@gmail.com>
2023-03-15 20:01:41 +0700

    VueJs Render Function

---
+++ lesson.32/js/app.js
@@ -0,0 +1,10 @@
+Vue.component('testcomponent',{
+    template : '<h1>Hello World</h1>',
+    data: function() {
+    },
+    methods:{
+    }
+ });
+ let vm = new Vue({
+    el: '#component_test'
+ });
